2dcbfc92afab127e6573a1e5f0585c11117a1361
Booker C. Bense <bbense@gmail.com>
2015-07-28 18:10:39 -0700

    wip

--- plugins/ohai_passwd.rb
+++ plugins/ohai_passwd.rb
@@ -9,12 +9,57 @@ Ohai.plugin(:PasswdMin) do
     str
   end
 
+  def parse_password_line(line,etc) 
+    line.chomp!
+    if line.chr == '+' 
+      parse_netgroup_line(line,etc)
+    else
+      entry = parse_pw_line(line)
+      etc[:passwd][entry[:name] = entry.except(:name) unless etc[:passwd].has_key?(entry[:name])
+    end 
+  end 
+
+  def parse_netgroup_line(line,etc)
+    #+user
+
+    #+@netgroup
+
+    #+:::::/afs/slac.stanford.edu/common/etc/use-NOT
+
+    #+
+  end 
+
+  def parse_pw_line(line)
+    entry = Mash.new
+    parsed_line = line.split(':')
+    entry[:name] = fix_encoding(parsed_line[0])
+    entry[:uid] = parsed_line[2].to_i
+    entry[:gid] = parsed_line[3].to_i
+    entry[:gecos] = parsed_line[4]
+    entry[:dir] = parsed_line[5]
+    entry[:shell] = parsed_line[6]
+    entry
+  end 
+
   collect_data do
     unless etc
       etc Mash.new
 
       etc[:passwd] = Mash.new
       etc[:group] = Mash.new
+      etc[:netgroups] = Mash.new
+
+      File.open("/etc/passwd", "r") do |f|
+       f.each_line do |line|
+          parse_passwd_line(line,etc)
+        end
+      end
+
+      File.open("/etc/group", "r") do |f|
+        f.each_line do |line|
+          parse_group_line(line,etc)
+        end
+      end
 
       Etc.passwd do |entry|
         user_passwd_entry = Mash.new(:dir => entry.dir, :gid => entry.gid, :uid => entry.uid, :shell => entry.shell, :gecos => entry.gecos)
@@ -39,4 +84,5 @@ Ohai.plugin(:PasswdMin) do
   collect_data(:windows) do
     # Etc returns nil on Windows
   end
+
 end
